fc1f854cee59e49fc869f9a324fce32fd4e120f3
Sebastian Schweizer <sebastian@schweizer.tel>
2017-01-28 00:43:39 +0100

    fix chunk loading

--- src/Mix.js
+++ src/Mix.js
@@ -87,11 +87,13 @@ class Mix {
      */
     output() {
         let filename = this.versioning ? '[name].[chunkhash].js' : '[name].js';
+        let chunkFilename = this.js.base + '/' + (this.versioning ? '[name].[chunkhash].js' : '[name].js');
 
         return {
             path: this.hmr ? '/' : this.publicPath,
             filename: filename,
-            publicPath: this.hmr ? 'http://localhost:8080/' : './'
+            chunkFilename: chunkFilename,
+            publicPath: this.hmr ? 'http://localhost:8080' : ''
         };
     }
 
