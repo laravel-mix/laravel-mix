7c7aec629ef7dff357c005c86e3c072c325dca15
forehalo <forehalo@gmail.com>
2017-02-05 01:05:52 +0800

    enable versioning of combined file

--- src/Concat.js
+++ src/Concat.js
@@ -1,5 +1,7 @@
 let chokidar = require('chokidar');
 let concatenate = require('concatenate');
+let mkdir = require('mkdirp');
+let md5 = require('md5');
 let File = require('./File');
 
 class Concat {
@@ -9,19 +11,33 @@ class Concat {
      * @param {object} events
      */
     constructor(events) {
-        this.files = [];
-
-        events.listen('build', this.run.bind(this));
+        this.combinations = [];
+        this.options = null;
+        this.events = events;
     }
 
+    /**
+     * Initialize the concatenate.
+     * 
+     * @param  {object} options
+     */
+    initialize(options) {
+        this.options = options;
+        this.events.listen('build', this.run.bind(this));
+        this.watch();
+    }
 
     /**
      * Add a set of files to be concatenated.
      *
      * @param {object} files
      */
-    add(files) {
-        this.files = this.files.concat(files);
+    add(combination) {
+        this.combinations = this.combinations.concat({
+            src: combination.src,
+            output: combination.output,
+            originOutput: combination.output
+        });
     }
 
 
@@ -31,15 +47,42 @@ class Concat {
     watch() {
         if (! process.argv.includes('--watch')) return;
 
-        let watchers = [].concat.apply(
-            [], this.files.map(set => set.src)
-        );
-
-        chokidar.watch(watchers, {
-            persistent: true
-        }).on('change', this.run.bind(this));
+        this.combinations.forEach(combination => {
+            chokidar.watch(combination.src, {
+                persistent: true
+            }).on('change', this.combine.bind(this, combination));
+        });
     }
 
+    /**
+     * Process combination.
+     * 
+     * @param  {object} combination
+     */
+    combine(combination) {
+        let parsedPath = File.find(combination.originOutput).parsePath();
+
+        mkdir.sync(parsedPath.base);
+
+        let all = concatenate.sync(combination.src, combination.originOutput);
+
+        if (this.options.versioning) {
+            let hash = md5(all);
+            // Remove previous name-hashed file in watch mode.
+            if (combination.originOutput !== combination.output) {
+                File.find(combination.output).delete();
+            }
+
+            combination.output = `${parsedPath.base}/${parsedPath.name}.${hash}${parsedPath.ext}`
+            File.rename(combination.originOutput, combination.output);
+        }
+
+        if (process.env.NODE_ENV === 'production') {
+            new File(combination.output).minify();
+        }
+
+        this.events.fire('combined', combination);
+    }
 
     /**
      * Combine the given files, or those from Mix.combine().
@@ -47,16 +90,8 @@ class Concat {
      * @param {array|null} files
      */
     run() {
-        this.files.forEach(file => {
-            require('mkdirp').sync(
-                File.find(file.output).parsePath().base
-            );
-
-           concatenate.sync(file.src, file.output);
-
-            if (process.env.NODE_ENV === 'production') {
-                new File(file.output).minify();
-            }
+        this.combinations.forEach(combination => {
+            this.combine(combination);
         });
 
         return this;
