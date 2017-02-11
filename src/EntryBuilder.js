632f1e6fdf082d9f4d0015b153f0f53e982ff39f
forehalo <forehalo@gmail.com>
2017-02-11 20:14:58 +0800

    add a mock entry for no mix.js() config

--- src/EntryBuilder.js
+++ src/EntryBuilder.js
@@ -54,25 +54,9 @@ class EntryBuilder {
      * user hasn't called mix.js().
      */
     addTemporaryScript() {
-        let file = new this.mix.File('mix-entry.js').write('');
+        let file = new this.mix.File(path.resolve(__dirname, 'mock-entry.js'));
 
         this.entry.add('mix', file.path());
-
-        this.mix.events.listen('build', stats => {
-            file.delete();
-
-            // If no mix.js() call was requested, we'll also need
-            // to delete the output script for the user. Since we
-            // won't know the exact name, we'll hunt it down.
-            let temporaryOutputFile = stats.toJson()
-                .assets
-                .find(asset => asset.chunkNames.includes('mix'))
-                .name;
-
-            this.mix.File.find(
-                path.join(this.mix.output().path, temporaryOutputFile)
-            ).delete();
-        });
 
         return this;
     }
