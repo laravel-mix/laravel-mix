34c3d476e91b00868ff3e692468166f0d92388e8
forehalo <forehalo@gmail.com>
2017-02-11 20:33:43 +0800

    always remove mock output file

--- src/EntryBuilder.js
+++ src/EntryBuilder.js
@@ -58,6 +58,20 @@ class EntryBuilder {
 
         this.entry.add('mix', file.path());
 
+        this.mix.events.listen('build', stats => {
+            // If no mix.js() call was requested, we'll also need
+            // to delete the output script for the user. Since we
+            // won't know the exact name, we'll hunt it down.
+            let temporaryOutputFile = stats.toJson()
+                .assets
+                .find(asset => asset.chunkNames.includes('mix'))
+                .name;
+
+            this.mix.File.find(
+                path.join(this.mix.output().path, temporaryOutputFile)
+            ).delete();
+        });
+
         return this;
     }
 
