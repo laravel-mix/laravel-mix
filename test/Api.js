0c7b7a23350eb4a9e13a0e494bc0a9e1c0d9e732
Thomas Praxl <thomas@macrominds.de>
2018-01-20 13:07:24 +0100

    Fix failing test introduced by commit a33885db7644

--- test/Api.js
+++ test/Api.js
@@ -295,12 +295,12 @@ test('mix.sourceMaps()', t => {
 
     // Sourcemaps should use a sensible type as the default for dev.
     t.is(mix, response);
-    t.is('inline-source-map', Config.sourcemaps);
+    t.is('eval-source-map', Config.sourcemaps);
 
     // For production builds, we should use a more performant type.
     Config.production = true;
     mix.sourceMaps();
-    t.is('cheap-source-map', Config.sourcemaps);
+    t.is('source-map', Config.sourcemaps);
 
     // But if the user specifies that sourcemaps shouldn't be built for
     // production, then we should disable it.
