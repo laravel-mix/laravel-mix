f2b64291e86135c6c5fd2783204034d35f6de001
Jordan Pittman <jordan@cryptica.me>
2018-03-04 10:57:41 -0500

    Reconfigure chunk splitting for Webpack 4

--- src/components/Extract.js
+++ src/components/Extract.js
@@ -1,4 +1,5 @@
 let webpack = require('webpack');
+let webpackMerge = require('webpack-merge')
 
 class Extract {
     /**
@@ -26,6 +27,63 @@ class Extract {
      */
     webpackEntry(entry) {
         this.entry = entry
+    }
+
+    webpackConfig(config) {
+        const newConfig = webpackMerge.smart(config, this.config());
+
+        config.optimization = newConfig.optimization
+    }
+
+    config() {
+        return {
+            optimization: {
+                // If we are extracting vendor libraries, then we also need
+                // to extract Webpack's manifest file to assist with caching.
+                runtimeChunk: {
+                    name: path.join(this.entry.base, 'manifest').replace(/\\/g, '/'),
+                },
+
+                splitChunks: {
+                    cacheGroups: {
+                        ...this.createCacheGroups(),
+                    },
+                },
+            },
+        }
+    }
+
+    createCacheGroups() {
+        const groups = {}
+
+        for (const [index, extraction] of this.extractions.entries()) {
+            groups[`vendor${index}`] = this.createCacheGroup(extraction)
+        }
+
+        return groups
+    }
+
+    createCacheGroup(extraction) {
+        const libsPattern = extraction.libs.join("|")
+        const pattern = new RegExp(`node_modules/(${libsPattern})`, "i")
+
+        return {
+            test: pattern,
+            name: this.extractionPath(extraction),
+            chunks: "all",
+            enforce: true,
+        }
+    }
+
+    extractionPath(extraction) {
+        if (extraction.output) {
+            return new File(extraction.output)
+                .pathFromPublic(Config.publicPath)
+                .replace(/\.js$/, '')
+                .replace(/\\/g, '/')
+        }
+
+        return path.join(this.entry.base, 'vendor').replace(/\\/g, '/');
     }
 }
 
