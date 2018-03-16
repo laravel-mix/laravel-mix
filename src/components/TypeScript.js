05dd0e2957b74cdfe32ac1e0a56e72ec2955ba35
Janek Thomaschewski <janek@jbbr.net>
2018-03-16 14:14:35 +0100

    Call parent webpackConfig in TypeScript component
    fixes absence of vue rules in TypeScript+Vue environments

--- src/components/TypeScript.js
+++ src/components/TypeScript.js
@@ -35,6 +35,8 @@ class TypeScript extends JavaScript {
      * @param {Object} config
      */
     webpackConfig(config) {
+        super.webpackConfig(config)
+
         config.resolve.extensions.push('.ts', '.tsx');
         config.resolve.alias['vue$'] = 'vue/dist/vue.esm.js';
     }
