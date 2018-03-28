8544376a6fe67d23e4ad5531cddb44773b162f4f
Антон Пинегин <pinegin@me.com>
2018-03-28 12:38:35 +0300

    Update webpack-rules.js
    division of the rules

--- src/builder/webpack-rules.js
+++ src/builder/webpack-rules.js
@@ -7,10 +7,47 @@ module.exports = function() {
         loaders: ['html-loader']
     });
 
+    // Add support for loading SVG graphic arts.
+    rules.push({
+        test: /^((?!font).)*\.svg$/,
+        loaders: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: path => {
+                        if (!/node_modules|bower_components/.test(path)) {
+                            return (
+                                Config.fileLoaderDirs.images +
+                                '/[name].[ext]?[hash]'
+                            );
+                        }
+
+                        return (
+                            Config.fileLoaderDirs.images +
+                            '/vendor/' +
+                            path
+                                .replace(/\\/g, '/')
+                                .replace(
+                                    /((.*(node_modules|bower_components))|images|image|img|assets)\//g,
+                                    ''
+                                ) +
+                            '?[hash]'
+                        );
+                    },
+                    publicPath: Config.resourceRoot
+                }
+            },
+
+            {
+                loader: 'img-loader',
+                options: Config.imgLoaderOptions
+            }
+        ]
+    });
+    
     // Add support for loading images.
     rules.push({
-        // only include svg that doesn't have font in the path or file name by using negative lookahead
-        test: /(\.(png|jpe?g|gif)$|^((?!font).)*\.svg$)/,
+        test: /\.(png|jpe?g|gif)$/,
         loaders: [
             {
                 loader: 'file-loader',
