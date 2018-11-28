57ebc296373875ece1faed9d4583120322159fdc
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-11-28 09:11:20 -0500

    Spacing

--- test/plugins/MixDefinitionsPlugin.js
+++ test/plugins/MixDefinitionsPlugin.js
@@ -3,12 +3,15 @@ import mix from '../../src/index';
 import MixDefinitionsPlugin from '../../src/webpackPlugins/MixDefinitionsPlugin';
 
 test('it fetches the MIX_ definitions properly', t => {
-    const plugin = new MixDefinitionsPlugin(
+    let plugin = new MixDefinitionsPlugin(
         path.resolve(__dirname, 'testing.env')
     );
-    const MIX_TESTING = '"123"';
-    const NODE_ENV = '"production"';
-    const definitions = plugin.getDefinitions({ NODE_ENV: 'production' });
+
+    let MIX_TESTING = '"123"';
+    let NODE_ENV = '"production"';
+
+    let definitions = plugin.getDefinitions({ NODE_ENV: 'production' });
+
     // Note that the definitions may contain more keys.
     // During a travis build with cached node modules, there's a MIX_ARCHIVES entry, for example.
     t.is(MIX_TESTING, definitions['process.env'].MIX_TESTING);
