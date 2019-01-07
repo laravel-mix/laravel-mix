623e3b38e6fa4d6ea8d456b0bf38f6c31f9d50ed
Andrey Berezhnoy <devil.andrey@gmail.com>
2019-01-07 21:46:13 +0200

    Switch to babel-preset-react-app

--- src/components/React.js
+++ src/components/React.js
@@ -5,7 +5,7 @@ class React extends JavaScript {
      * Required dependencies for the component.
      */
     dependencies() {
-        return ['@babel/preset-react'];
+        return ['@babel/preset-react-app'];
     }
 
     /**
@@ -13,7 +13,7 @@ class React extends JavaScript {
      */
     babelConfig() {
         return {
-            presets: ['@babel/preset-react']
+            presets: ['@babel/preset-react-app']
         };
     }
 }
