8978ccf030969ed07d82bacccd3593eea9116a4b
Ben Taber <ben.taber@gmail.com>
2019-11-20 09:08:19 -0700

    Add cssnano to postcss plugins

--- src/components/Preprocessor.js
+++ src/components/Preprocessor.js
@@ -66,9 +66,11 @@ class Preprocessor {
                                 }
 
                                 if (Mix.inProduction()) {
-                                    require('cssnano')({
-                                        preset: 'default'
-                                    });
+                                    plugins.push(
+                                        require('cssnano')({
+                                            preset: 'default'
+                                        })
+                                    );
                                 }
 
                                 return plugins;
