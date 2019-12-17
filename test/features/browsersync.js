ddd3834eae01bb2d40fe186d8a72ab23d8830f51
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2019-12-17 11:39:37 -0500

    Browsersync snippet regex for lookbehinds (#2227)

--- test/features/browsersync.js
+++ test/features/browsersync.js
@@ -1,5 +1,6 @@
 import mix from './helpers/setup';
 import mockRequire from 'mock-require';
+import Browsersync from '../../src/components/Browsersync';
 
 mockRequire(
     'browser-sync-webpack-plugin',
@@ -20,4 +21,24 @@ test.serial.cb('it handles Browsersync reloading', t => {
             )
         );
     });
+});
+
+test.serial.cb('it injects the snippet in the right place', t => {
+    let regex = new Browsersync().regex();
+
+    t.is(regex.exec(`<div></div>`), null);
+    t.is(regex.exec(`<body></body>`).index, 6);
+    t.is(regex.exec(`<BODY></BODY>`).index, 6);
+    t.is(regex.exec(`<pre></pre>`).index, 5);
+    t.is(regex.exec(`<body><pre></pre></body>`).index, 17);
+    t.is(
+        regex.exec(`
+            <body>
+                <pre></pre>
+                <div></div>
+                <pre></pre>
+            </body>
+        `).index,
+        116
+    );
 });
