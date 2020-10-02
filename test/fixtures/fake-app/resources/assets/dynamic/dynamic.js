4c7489e149ad5300c85987cddcbe5968b5f1d38d
Jordan Pittman <jordan@cryptica.me>
2020-10-01 21:10:46 -0400

    Test named imports
    Also fix the code as dynamic import modules expose named exports as individual properties and the default export via the `default` property.

--- test/fixtures/fake-app/resources/assets/dynamic/dynamic.js
+++ test/fixtures/fake-app/resources/assets/dynamic/dynamic.js
@@ -1,1 +1,7 @@
-import('./dynamic-module').then(module => module.init());
+import('./dynamic-module').then(module => module.default.init());
+import(/* webpackChunkName: 'named' */ './dynamic-module-named').then(module =>
+    module.default.init()
+);
+import(/* webpackChunkName: '/js/absolute' */ './dynamic-module-absolute').then(
+    module => module.default.init()
+);
