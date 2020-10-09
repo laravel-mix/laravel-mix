ad7b62d2f8f0e85f699219e855de5787f55aef65
Jordan Pittman <jordan@cryptica.me>
2020-10-09 17:13:31 -0400

    Fix file paths for dynamic imports

--- test/fixtures/app/src/extract/app.js
+++ test/fixtures/app/src/extract/app.js
@@ -5,5 +5,10 @@ new Vue({
     components: {
         VueSplit: () =>
             import(/* webpackChunkName: '/js/split' */ './VueSplit.vue')
+    },
+
+    async mounted() {
+        const mod = await import('./dynamic');
+        mod.default();
     }
 }).$mount('#app');
