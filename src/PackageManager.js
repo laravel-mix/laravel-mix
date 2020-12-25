eff79d44f86a9888d739b9354a5ced76a4f91ea8
Chan, Danny <danny50610@yahoo.com.tw>
2020-12-25 20:54:56 +0800

    Only check filename part. (#2693)
    * Only check filename part.
    * Also detect yarn via yarn.lock

--- src/PackageManager.js
+++ src/PackageManager.js
@@ -1,12 +1,21 @@
+const File = require('./File');
+const Paths = require('./Paths');
+
 class PackageManager {
     static detect() {
         const execPath = process.env.npm_execpath || '';
 
-        if (execPath.endsWith('bin/yarn.js')) {
+        if (execPath.endsWith('yarn.js') || PackageManager.hasYarnLockFile()) {
             return 'yarn';
         }
 
         return 'npm';
+    }
+
+    static hasYarnLockFile() {
+        const paths = new Paths();
+
+        return File.exists(paths.root('yarn.lock'));
     }
 }
 
