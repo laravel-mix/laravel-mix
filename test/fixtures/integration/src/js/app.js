da57681a3c6a17e2d3b433c6c0aeff919104f562
Jordan Pittman <jordan@cryptica.me>
2021-01-02 09:58:37 -0500

    Add react css modules test

--- test/fixtures/integration/src/js/app.js
+++ test/fixtures/integration/src/js/app.js
@@ -1,6 +1,8 @@
 import './node-browser-polyfills';
 import { setupVueApp } from './app-vue';
+import { setupReactApp } from './app-react';
 
 console.log('loaded: app.js');
 
 setupVueApp();
+setupReactApp();
