d2554ed842c79b009c0846c7a77c1188cc5b2995
Jordan Pittman <jordan@cryptica.me>
2021-01-06 17:45:46 -0500

    Fix mix.webpackConfig usage

--- src/components/WebpackConfig.js
+++ src/components/WebpackConfig.js
@@ -2,7 +2,7 @@ let merge = require('../builder/MergeWebpackConfig');
 
 class WebpackConfig {
     register(config) {
-        mix.override(webpackConfig => {
+        global.Mix.api.override(webpackConfig => {
             config = typeof config == 'function' ? config(webpackConfig) : config;
 
             Object.assign(webpackConfig, merge(webpackConfig, config));
