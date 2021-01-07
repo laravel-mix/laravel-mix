c4f3ee21c51de92836a364503ab8ab7eacf167cf
Jordan Pittman <jordan@cryptica.me>
2021-01-07 07:28:25 -0500

    Add type info

--- src/components/WebpackConfig.js
+++ src/components/WebpackConfig.js
@@ -1,6 +1,13 @@
 let merge = require('../builder/MergeWebpackConfig');
 
+/** @typedef {import('webpack').Configuration} Configuration */
+/** @typedef {import('webpack')} webpack */
+
 class WebpackConfig {
+    /**
+     *
+     * @param {((webpack: webpack, config: Configuration) => Configuration)|Configuration} config
+     */
     register(config) {
         global.Mix.api.override(webpackConfig => {
             config = typeof config == 'function' ? config(webpackConfig) : config;
