a2492285bd998ad05e26dac5c3a2b186a4bafc2c
Jordan Pittman <jordan@cryptica.me>
2021-01-07 08:32:06 -0500

    Fix breaking change in mix.webpackConfig (#2751)
    * Add type info
    
    * Fix breaking change in mix.webpackConfig

--- src/builder/MergeWebpackConfig.js
+++ src/builder/MergeWebpackConfig.js
@@ -1,5 +1,12 @@
 let { mergeSmart } = require('./MergeSmart');
 
+/** @typedef {import('webpack').Configuration} Configuration */
+
+/**
+ *
+ * @param {Configuration} configA
+ * @param {Configuration} configB
+ */
 module.exports = (configA, configB) => {
     return mergeSmart(configA, configB);
 };
--- src/components/WebpackConfig.js
+++ src/components/WebpackConfig.js
@@ -1,9 +1,18 @@
 let merge = require('../builder/MergeWebpackConfig');
+let webpack = require('webpack');
+
+/** @typedef {import('webpack').Configuration} Configuration */
+/** @typedef {import('webpack')} webpack */
 
 class WebpackConfig {
+    /**
+     *
+     * @param {((webpack: webpack, config: Configuration) => Configuration)|Configuration} config
+     */
     register(config) {
         global.Mix.api.override(webpackConfig => {
-            config = typeof config == 'function' ? config(webpackConfig) : config;
+            config =
+                typeof config == 'function' ? config(webpack, webpackConfig) : config;
 
             Object.assign(webpackConfig, merge(webpackConfig, config));
         });
