d77ef0b84aa6b7045eb21f1b0a25fd18aafb8e80
Amen Arbi <aarbi@vistaprint.com>
2021-04-19 08:29:19 +0100

    Update the split chunks list
    To pull rendered CSS out of React JSX files

--- src/components/React.js
+++ src/components/React.js
@@ -1,6 +1,14 @@
 const semver = require('semver');
+let { Chunks } = require('../Chunks');
 
 class React {
+    /**
+     * Create a new component instance.
+     */
+    constructor() {
+        this.chunks = Chunks.instance();
+    }
+
     /**
      * Required dependencies for the component.
      */
@@ -90,6 +98,79 @@ class React {
      */
     library() {
         return require('react');
+    }
+
+    /**
+     * Update CSS chunks to extract React styles
+     */
+    updateChunks() {
+        if (this.options.extractStyles === false) {
+            return;
+        }
+
+        this.chunks.add(
+            'styles-js',
+            this.styleChunkName(),
+            [/.(j|t)s$/, module => module.type === 'css/mini-extract'],
+            {
+                chunks: 'all',
+                enforce: true,
+                type: 'css/mini-extract'
+            }
+        );
+
+        this.chunks.add(
+            'styles-jsx',
+            this.styleChunkName(),
+            [/.(j|t)sx?$/, module => module.type === 'css/mini-extract'],
+            {
+                chunks: 'all',
+                enforce: true,
+                type: 'css/mini-extract'
+            }
+        );
+    }
+
+    /**
+     * Get the name of the style chunk.
+     *
+     * @returns {string}
+     */
+    styleChunkName() {
+        // If the user set extractStyles: true, we'll try
+        // to append the React styles to an existing CSS chunk.
+        if (this.options.extractStyles === true) {
+            let chunk = this.chunks.find((chunk, id) => id.startsWith('styles-'));
+
+            if (chunk) {
+                return chunk.name;
+            }
+        }
+
+        return this.extractFile().relativePathWithoutExtension();
+    }
+
+    /**
+     * Get a new File instance for the extracted file.
+     *
+     * @returns {File}
+     */
+    extractFile() {
+        return new File(this.extractFileName());
+    }
+
+    /**
+     * Determine the extract file name.
+     *
+     * @return {string}
+     */
+    extractFileName() {
+        let fileName =
+            typeof this.options.extractStyles === 'string'
+                ? this.options.extractStyles
+                : '/css/app-styles.css';
+
+        return fileName.replace(Config.publicPath, '').replace(/^\//, '');
     }
 }
 
