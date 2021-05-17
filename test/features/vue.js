ad681c1f2c30847b4799e7a0f040e90a32e918ca
Jordan Pittman <jordan@cryptica.me>
2021-05-17 19:16:54 -0400

    Disable vue style loader

--- test/features/vue.js
+++ test/features/vue.js
@@ -38,13 +38,13 @@ test('it knows the Vue 2 compiler name', t => {
     t.true(dependencies.includes('vue-template-compiler'));
 });
 
-test('it switches to vue-style-loader when not extracting styles', async t => {
+test('DISABLED: it switches to vue-style-loader when not extracting styles', async t => {
     mix.vue({ version: 2, extractStyles: false });
 
     const config = await webpack.buildConfig();
 
-    assert.hasWebpackLoader(t, config, 'vue-style-loader');
-    assert.doesNotHaveWebpackLoader(t, config, 'style-loader');
+    assert.doesNotHaveWebpackLoader(t, config, 'vue-style-loader');
+    assert.hasWebpackLoader(t, config, 'style-loader');
     assert.doesNotHaveWebpackLoader(t, config, /mini-css-extract-plugin/);
 });
 
