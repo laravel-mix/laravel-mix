28cba1354505c4e769e73b95f1eab6ec2666559a
Filipe Voges <filipe.vogesh@gmail.com>
2021-09-24 09:13:58 -0300

    Add a New Function isset()
    Added new help function in Javascript based on PHP's isset() function

--- src/helpers.js
+++ src/helpers.js
@@ -31,6 +31,43 @@ if (!Array.prototype.hasOwnProperty('tap')) {
 }
 
 /**
+ * Determine if a variable is declared and is different than null
+ * Based on PHP isset() Function
+ * @see https://www.php.net/manual/en/function.isset.php
+ * @see https://www.npmjs.com/package/@filipevoges/isset
+ *
+ * @param key
+ * @return bool
+ *
+ * @author Filipe Voges <filipe.vogesh@gmail.com>
+ * @version 1.0.1
+ * @since 2021-09-17
+ */
+ if (!Object.hasOwnProperty('isset')) {
+	Object.prototype.isset = function() {
+		if(arguments.length > 2) {
+			throw new Error('Too many arguments');
+		}
+		let obj = arguments[0];
+		let keyArgCheck = 1;
+		if(Object.hasOwnProperty('global') && typeof this.global === 'undefined') {
+			obj = this;
+			keyArgCheck = 0;
+		}
+		if(typeof obj === 'undefined' || obj === null) {
+			return false;
+		}
+		let argCheck = arguments[keyArgCheck];
+		if (typeof argCheck !== 'undefined') {
+			if (typeof obj[argCheck] === 'undefined' || obj[argCheck] === null) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+/**
  * Add wrap to arrays.
  *
  * @param {*}    val
