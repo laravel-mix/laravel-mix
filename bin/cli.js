03349641368c302b1ff3c2228d089963ac9896ac
Jordan Pittman <jordan@cryptica.me>
2021-10-28 10:26:38 -0400

    Fix usage on Node 17 (#3138)

--- bin/cli.js
+++ bin/cli.js
@@ -81,6 +81,10 @@ async function executeScript(cmd, opts, args = []) {
         MIX_FILE: opts.mixConfig
     };
 
+    const nodeEnv = process.version.startsWith('v17.')
+        ? { NODE_OPTIONS: process.env.NODE_OPTIONS || `--openssl-legacy-provider` }
+        : {};
+
     if (isTesting()) {
         process.stdout.write(
             JSON.stringify({
@@ -98,6 +102,7 @@ async function executeScript(cmd, opts, args = []) {
             shell: true,
             env: {
                 ...process.env,
+                ...nodeEnv,
                 ...scriptEnv
             }
         });
