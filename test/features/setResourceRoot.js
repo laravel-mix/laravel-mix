68df1776bc9b74a2e64a1ee5bc30d981fdc047f8
Jordan Pittman <jordan@cryptica.me>
2021-10-29 09:52:14 -0400

    Add resource root rewriting test

--- test/features/setResourceRoot.js
+++ test/features/setResourceRoot.js
@@ -1,6 +1,9 @@
 import test from 'ava';
 
 import { mix, Mix } from '../helpers/mix.js';
+import webpack from '../helpers/webpack.js';
+import assert from '../helpers/assertions.js';
+import File from '../../src/File.js';
 
 test('mix.setResourceRoot()', t => {
     let response = mix.setResourceRoot('some/path');
@@ -8,4 +11,23 @@ test('mix.setResourceRoot()', t => {
     t.deepEqual(mix, response);
 
     t.is('some/path', Mix.config.resourceRoot);
+});
+
+test('mix.setResourceRoot() rewrites processed asset urls', async t => {
+    mix.setResourceRoot('https://www.example.com/');
+    mix.postCss(`test/fixtures/app/src/css/app-and-image.css`, 'css');
+
+    await webpack.compile();
+
+    t.true(File.exists(`test/fixtures/app/dist/css/app-and-image.css`));
+    t.true(File.exists(`test/fixtures/app/dist/images/img.svg`));
+
+    assert.fileMatchesCss(
+        `test/fixtures/app/dist/css/app-and-image.css`,
+        `.app {
+            color: red;
+            background-image: url(https://www.example.com/images/img.svg?66162863e7583212a5d4fd6cdc2426ed);
+        }`,
+        t
+    );
 });
