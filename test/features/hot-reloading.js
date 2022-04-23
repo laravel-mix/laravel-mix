08f17c49e9dc466786383b831823f988198f1cf2
Jordan Pittman <jordan@cryptica.me>
2022-04-22 21:52:15 -0400

    Add test verifying HMR options are read from options

--- test/features/hot-reloading.js
+++ test/features/hot-reloading.js
@@ -21,3 +21,25 @@ test('it creates a file to mark a request for hot reloading', async t => {
 
     assert(t).file(hotFilePath).exists();
 });
+
+test('it reads HMR details from options', async t => {
+    const { mix, Mix, assert } = context(t);
+
+    mix.options({
+        hmr: true,
+        hmrOptions: {
+            https: true,
+            host: 'example.com',
+            port: 1337,
+        }
+    });
+
+    assert(t).file(hotFilePath).absent();
+
+    // Mix should listen for the "init" event before checking
+    // if the user desires hot reloading.
+    await Mix.init();
+
+    assert(t).file(hotFilePath).exists();
+    assert(t).file(hotFilePath).contains('https://example.com:1337')
+});
