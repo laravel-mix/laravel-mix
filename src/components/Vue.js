5e273562e79065845561c09a2fcc9b9099284185
Brandon Surowiec <BrandonSurowiec@users.noreply.github.com>
2022-06-08 21:37:27 -0400

    Fix Webpack 'vue-loader' Module not found error (#3275)

--- src/components/Vue.js
+++ src/components/Vue.js
@@ -64,7 +64,7 @@ module.exports = class Vue extends Component {
 
         let dependencies = [
             this.version === 2 ? 'vue-template-compiler' : '@vue/compiler-sfc',
-            this.version === 2 ? 'vue-loader@^15.9.7' : 'vue-loader@^16.2.0'
+            this.version === 2 ? 'vue-loader@^15.9.8' : 'vue-loader@^16.2.0'
         ];
 
         if (this.options.extractStyles && this.options.globalStyles) {
